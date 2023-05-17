f0cc3c5d42ac1ac00cd87dfac526343c681dd186
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2023-05-17 11:45:22 -0300

    feature: create route user

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,37 @@
-import fastify from 'fastify'
-
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
-
-console.log(prisma)
+import { z } from 'zod'
+import { prisma } from './lib/prisma'
+import { env } from './env'
+import { fastify } from 'fastify'
 
 export const app = fastify({
   logger: true,
 })
+
+app.post('/user', async (request, reply) => {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { email, name, password } = registerBodySchema.parse(request.body)
+
+  await prisma.user.create({
+    data: {
+      name,
+      password_hash: password,
+      email,
+    },
+  })
+
+  return reply.status(201).send()
+})
+
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('listening server run on port 3333')
+  })
