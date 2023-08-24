8feb37d705076c61361b3390dbe27ba94a1e4696
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2023-08-24 13:05:45 -0300

    feat: create method gym and test use case

---
+++ src/use-cases/gym/index.ts
@@ -0,0 +1,36 @@
+import { Gym } from '@prisma/client'
+import { GymsRepository } from '@/repositories/prisma/gyms-repository'
+
+interface CreateGymUseCaseRequest {
+  title: string
+  description: string | null
+  phone: string | null
+  latitude: number
+  longitude: number
+}
+
+interface CreateGymUseCaseResponse {
+  gym: Gym
+}
+
+export class CreateGymUseCase {
+  constructor(private gymsRepository: GymsRepository) {}
+
+  async execute({
+    description,
+    latitude,
+    longitude,
+    phone,
+    title,
+  }: CreateGymUseCaseRequest): Promise<CreateGymUseCaseResponse> {
+    const gym = await this.gymsRepository.create({
+      latitude,
+      longitude,
+      title,
+      description,
+      phone,
+    })
+
+    return { gym }
+  }
+}
