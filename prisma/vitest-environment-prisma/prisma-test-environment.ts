52602e90ee9c4bea102935b30b5277676e1a5bef
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2023-12-13 15:58:37 -0300

    feat: generate database test setup

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,10 +1,43 @@
+import 'dotenv/config'
+import { randomUUID } from 'node:crypto'
 import { Environment } from 'vitest'
+import { execSync } from 'node:child_process'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+// postgresql://docker:docker@localhost:5432/api_solid
+
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Database environment variable not exist!')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
 
 export default <Environment>{
   name: 'prisma',
   async setup() {
+    const schema = randomUUID()
+    const databaseURL = generateDatabaseURL(schema)
+
+    process.env.DATABASE_URL = databaseURL
+
+    execSync('npx prisma migrate deploy') // executar apenas as migrates do banco de dados
+
     return {
-      teardown() {},
+      async teardown() {
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
+
+        await prisma.$disconnect()
+      },
     }
   },
 }
