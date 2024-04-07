3cdb4a425e70a9e4a7b602b66bfddedb9b49629f
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2024-04-07 09:00:18 -0300

    feat: create routes gyms and users and implement gyms controllers

---
+++ src/http/controllers/users/routes.ts
@@ -0,0 +1,14 @@
+import { FastifyInstance } from 'fastify'
+import { registerUser } from './register'
+import { authenticateUser } from './authenticate'
+import { profile } from './profile'
+import { verifyJWT } from '@/http/middlewares/verify-jwt'
+
+export async function usersRoutes(app: FastifyInstance) {
+  app.post('/users', registerUser)
+
+  app.post('/session', authenticateUser)
+
+  /** Authenticated */
+  app.get('/me', { onRequest: [verifyJWT] }, profile)
+}
