57a8d530778eae38fed5227b5d8d0f0b621fb8dd
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2024-05-09 16:03:39 -0300

    feat: remove duo listen app

--- src/app.ts
+++ src/app.ts
@@ -43,12 +43,3 @@ app.setErrorHandler((error, _, reply) => {
 
   return reply.status(500).send({ message: 'Internal Server Error!' })
 })
-
-app
-  .listen({
-    host: '0.0.0.0',
-    port: env.PORT,
-  })
-  .then(() => {
-    console.log('listening server run on port 3333')
-  })
